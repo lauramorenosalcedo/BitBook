101e1bcc0e03b5fafbf58ba6df616b743f2dfaf7
lauramoreno <154345923+lauramorenosalcedo@users.noreply.github.com>
2024-02-29 20:52:09 +0100

    Add files via upload

---
+++ BitBook/src/main/java/com/example/bitbook/BitBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.bitbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitBookApplication.class, args);
+    }
+
+}
---
+++ BitBook/src/test/java/com/example/bitbook/BitBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bitbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
